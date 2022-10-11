16e3420d87186bed6f120020de1dc807d56a3af3
Alexander Martinovich <alexander_martinovich@msn.com>
2022-10-12 02:18:16 +0300

    feat: 2 more test complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity) return false;
+  if (typeof sampleActivity !== 'string') return false;
+  if (sampleActivity > MODERN_ACTIVITY) return false;
+  // let number = Number(sampleActivity);
+  // if (isFinite(number)) return false;
+  if (isNaN(+sampleActivity)) return false;
+  if (+sampleActivity <= 0) return false;
+  let result = Math.ceil(Math.log(+sampleActivity / MODERN_ACTIVITY) / -0.693 * HALF_LIFE_PERIOD);
+  return result;
 }
 
 module.exports = {
