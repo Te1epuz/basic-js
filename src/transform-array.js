86e2544968ca98c9ca566166ae180c78de683ac4
Alexander Martinovich <alexander_martinovich@msn.com>
2022-10-13 02:40:51 +0300

    feat: +4 more task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) { throw Error(`'arr' parameter must be an instance of the Array!`); }
+  let result = [];
+  let discarded = false;
+  for (let i = 0; i <= arr.length - 1; i++) {
+
+    if (arr[i] === '--discard-next') {
+      i++;
+      discarded = true;
+      continue;
+    }
+    else if (arr[i] === '--discard-prev') {
+      if (discarded === false) {
+        result.pop();
+      }
+    }
+    else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) {
+        result.push(arr[i+1], arr[i+1]);
+      }
+      i++;
+    }
+    else if (arr[i] === '--double-prev') {
+      if (i > 0) {
+        if (discarded === false) {
+          result.push(arr[i-1]);
+        }
+      };
+    }
+    else {
+      result.push(arr[i]);
+    }
+    discarded = false;
+
+  }
+  return result;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,25 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {return 'Unable to determine the time of year!'}
+  if (toString.call(date) !== '[object Date]') {throw Error('Invalid date!');}
+  if (Object.keys(date).length > 0) {throw Error('Invalid date!');}
+  let result = '';
+  if (date.getMonth() < 2 || date.getMonth() === 11) {result = 'winter'}
+  else if (date.getMonth() < 5) {result = 'spring'}
+  else if (date.getMonth() < 8) {result = 'summer'}
+  else {result = 'fall'}
+  return result;
 }
 
 module.exports = {
