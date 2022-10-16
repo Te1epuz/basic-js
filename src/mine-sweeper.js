d1f2c084cc8a947eb9a590efc1d96613496a13ea
Alexander Martinovich <alexander_martinovich@msn.com>
2022-10-16 15:11:25 +0300

    feat: add last subtasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper(matrix) {
+  let result = [];
+  for (let y = 0; y < matrix.length; y++){
+    result[y] = [];
+    for (let x = 0; x < matrix[0].length; x++){ 
+      if (matrix[y][x] === true) {
+        result[y][x] = 1;}
+      else { 
+        result[y][x] = 0;
+
+        if (y > 0) {
+          if (matrix[y-1][x-1]) {result[y][x] += 1}
+          if (matrix[y-1][x]) {result[y][x] += 1}
+          if (matrix[y-1][x+1]) {result[y][x] += 1}          
+        }
+        
+        if (matrix[y][x-1]) {result[y][x] += 1}
+        if (matrix[y][x]) {result[y][x] += 1}
+        if (matrix[y][x+1]) {result[y][x] += 1}
+        
+        if (y < matrix.length - 1) {
+          if (matrix[y+1][x-1]) {result[y][x] += 1}
+          if (matrix[y+1][x]) {result[y][x] += 1}
+          if (matrix[y+1][x+1]) {result[y][x] += 1}          
+        }
+      }
+    }    
+  }
+  return result;
 }
 
 module.exports = {
